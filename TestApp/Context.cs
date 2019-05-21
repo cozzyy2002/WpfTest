25a3c1ae33f3f96c6a1dac5580cfa838605f5955
Koji Yano <cozzyy2002@gmail.com>
2019-05-21 18:41:50 +0900

    Add TestType enum and ComboBox which shows the enum members. Add TabControl contains 2 ListViews in 2 TabItemss.

--- TestApp/Context.cs
+++ TestApp/Context.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Input;
@@ -10,6 +11,23 @@ namespace TestApp.Model
         public Context()
         {
             TestCommand = new TestCommand();
+            TestCommand.CanExecuteChanged += delegate (object sender, EventArgs e)
+            {
+                Trace.WriteLine(string.Format("TestCommand.CanExecute() returns {0}", TestCommand.CanExecute(null)));
+            };
+            TestType = TestType.One;
+
+            ListItemOneList = new ObservableCollection<ListItemOne>() {
+                new ListItemOne() { Name = "John", Age = 20 },
+                new ListItemOne() { Name = "Gorge", Age = 21 }
+            };
+
+            ListItemTwoList = new ObservableCollection<ListItemTwo>()
+            {
+                new ListItemTwo() { Name = "Apple" },
+                new ListItemTwo() { Name = "Orange" },
+                new ListItemTwo() { Name = "Banana" }
+            };
         }
 
         public string Text
@@ -19,19 +37,88 @@ namespace TestApp.Model
         }
         public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(Context));
 
+        public TestType TestType
+        {
+            get { return (TestType)GetValue(TestTypeProperty); }
+            set { SetValue(TestTypeProperty, value); }
+        }
+        public static readonly DependencyProperty TestTypeProperty = DependencyProperty.Register("TestType", typeof(TestType), typeof(Context));
+
+        public bool TestCheckBox
+        {
+            get { return (bool)GetValue(TestCheckBoxProperty); }
+            set { SetValue(TestCheckBoxProperty, value); }
+        }
+        public static readonly DependencyProperty TestCheckBoxProperty = DependencyProperty.Register("TestCheckBox", typeof(bool), typeof(Context),
+            new PropertyMetadata(TestCheckBoxChanged));
+
+        protected static void TestCheckBoxChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var _this = d as Context;
+            _this?.TestCommand.OnCanExecuteChanged((bool)e.NewValue);
+        }
+
         public TestCommand TestCommand { get; set; }
+
+        public ObservableCollection<ListItemOne> ListItemOneList { get; protected set; }
+        public ObservableCollection<ListItemTwo> ListItemTwoList { get; protected set; }
+
+        public ListItemBase SelectedListItem
+        {
+            get { return (ListItemBase)GetValue(SelectedListItemProperty); }
+            set { SetValue(SelectedListItemProperty, value); }
+        }
+        public static readonly DependencyProperty SelectedListItemProperty = DependencyProperty.Register("SelectedListItem", typeof(ListItemBase), typeof(Context),
+            new PropertyMetadata(ListItemChanged));
+
+        protected static void ListItemChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var item = e.NewValue as ListItemBase;
+            var msg = string.Format("ListItemChanged(Type={0}, Name={1})", item?.Type, item?.Name);
+            Trace.WriteLine(msg);
+        }
+    }
+
+    public enum TestType
+    {
+        Zero,
+        One,
+        Two
+    }
+
+    public class ListItemBase
+    {
+        public virtual TestType Type { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ListItemOne : ListItemBase
+    {
+        public override TestType Type => TestType.One;
+        public int Age { get; set; }
+    }
+
+    public class ListItemTwo : ListItemBase
+    {
+        public override TestType Type => TestType.Two;
     }
 
     public class TestCommand : ICommand
     {
         public event EventHandler CanExecuteChanged;
-        public void OnCanExecuteChanged() { CanExecuteChanged?.Invoke(this, new EventArgs()); }
+        public void OnCanExecuteChanged(bool _CanExecute)
+        {
+            this._CanExecute = _CanExecute;
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
 
         public bool CanExecute(object parameter)
         {
             Trace.WriteLine("TestCommand.CanExecute()");
-            return true;
+            return _CanExecute;
         }
+
+        protected bool _CanExecute;
 
         public void Execute(object parameter)
         {
--- TestApp/MainWindow.xaml.cs
+++ TestApp/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace TestApp
 {
@@ -14,6 +18,47 @@ namespace TestApp
             Context = new Model.Context();
 
             InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+            Closed += MainWindow_Closed;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            log("MainWindow_Loaded({0},{1})", sender, e);
+        }
+
+        private void MainWindow_Closed(object sender, System.EventArgs e)
+        {
+            log("MainWindow_Closed({0},{1})", sender, e);
+        }
+
+        protected void log(string format, params object[] args)
+        {
+            var msg = string.Format(format, args);
+            Trace.WriteLine(msg);
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Context == null) return;
+
+            var tabControl = sender as TabControl;
+            Selector sel = null;
+            switch(tabControl.SelectedIndex)
+            {
+            case 0:
+                sel = ListItemOneList;
+                break;
+            case 1:
+                sel = ListItemTwoList;
+                break;
+            }
+
+            var item = sel?.SelectedItem as TestApp.Model.ListItemBase;
+            Context.SelectedListItem = item;
+            var msg = string.Format("TabControl_SelectionChanged(Type={0}, Name={1})", item?.Type, item?.Name);
+            Trace.WriteLine(msg);
         }
     }
 }
