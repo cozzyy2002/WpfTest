9f6e7a2e392458aa3e8125647eb3da8303e1e5a2
Koji Yano <cozzyy2002@gmail.com>
2019-05-21 18:46:03 +0900

    Add UnitTest project.

---
+++ UnitTest/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace UnitTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            GoCommand = new GoCommand();
+
+            InitializeComponent();
+        }
+
+        public string TargetFileName
+        {
+            get { return (string)GetValue(TargetFileNameProperty); }
+            set { SetValue(TargetFileNameProperty, value); }
+        }
+        public static readonly DependencyProperty TargetFileNameProperty = DependencyProperty.Register("TargetFileName", typeof(string), typeof(MainWindow));
+
+        public GoCommand GoCommand { get; protected set; }
+    }
+
+    public class GoCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
